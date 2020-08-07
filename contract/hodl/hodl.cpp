ce25fc3432800afb767310906900b94938c411fc
chef-danny-d <dpapp001@odu.edu>
2020-08-07 00:16:52 +0000

    final projet

---
+++ contract/hodl/hodl.cpp
@@ -0,0 +1,90 @@
+#include <eosio/eosio.hpp>
+#include <eosio/print.hpp>
+#include <eosio/asset.hpp>
+#include <eosio/system.hpp>
+
+using namespace eosio;
+
+class [[eosio::contract("hodl")]] hodl : public eosio::contract {
+  private:
+    uint32_t the_party = current_time_point().sec_since_epoch();
+    //static const uint32_t the_party = 1645525342;
+    const symbol hodl_symbol;
+
+    struct [[eosio::table]] balance
+    {
+      eosio::asset funds;
+      uint64_t primary_key() const { return funds.symbol.raw(); }
+    };
+
+    using balance_table = eosio::multi_index<"balance"_n, balance>;
+
+    uint32_t now() {
+      return current_time_point().sec_since_epoch();
+    }
+
+    int offset(uint32_t the_party) {
+        print("Offset time is: ", the_party + 120, "\n");
+        uint32_t off = the_party + 120;
+        return off;
+    }
+
+  public:
+    using contract::contract;
+
+    hodl(name receiver, name code, datastream<const char *> ds) : contract(receiver, code, ds),hodl_symbol("SYS", 4){}
+
+    [[eosio::on_notify("eosio.token::transfer")]]
+    void deposit(name hodler, name to, eosio::asset quantity, std::string memo) {
+      if (hodler == get_self() || to != get_self()){
+        return;
+      }
+
+      check(now() < offset(the_party), "Too late");
+      check(quantity.amount > 0, "When pigs fly");
+      check(quantity.symbol == hodl_symbol, "These are not the droids you are looking for.");
+
+      balance_table balance(get_self(), hodler.value);
+      auto hodl_it = balance.find(hodl_symbol.raw());
+
+      if (hodl_it != balance.end())
+        balance.modify(hodl_it, get_self(), [&](auto &row) {
+          row.funds += quantity;
+        });
+      else{
+        balance.emplace(get_self(), [&](auto &row) {
+          row.funds = quantity;
+        });
+        offset(the_party);
+      }
+    }
+
+    [[eosio::action]]
+    void party(name hodler){
+      //Check the authority of hodlder
+      require_auth(hodler);
+
+      if(now() > offset(the_party)){
+          offset(the_party);
+          balance_table balance(get_self(), hodler.value);
+            auto hodl_it = balance.find(hodl_symbol.raw());
+
+            // //Make sure the holder is in the table
+            check(hodl_it != balance.end(), "You're not allowed to party");
+
+            action{
+                permission_level{get_self(), "active"_n},
+                "eosio.token"_n,
+                "transfer"_n,
+                std::make_tuple(get_self(), hodler, hodl_it->funds, std::string("Party! Your hodl is free."))
+            }.send();
+
+            balance.erase(hodl_it);
+      }
+      else{
+        // //Check the current time has pass the the party time
+        offset(the_party);
+        check(now() < the_party, "Hold your horses");
+      }     
+    }
+};
